a33d5c777fd2c2448957bfee402bbe36be861f33
Harsha542 <22r01a0542@cmrithyderabad.edu.in>
2026-06-05 00:18:53 +0530

    second commit

--- singlerowfunc.sql
+++ singlerowfunc.sql
@@ -1,9 +1,9 @@
 create table student(id int,name varchar(20),dob date,email varchar(20) default "collage@gmail.com" , mob bigint);
 
-insert into student values(18,"Harsha",20-04-2004,"harsha@gmail.com",900101010),
-                          (19,"virat",20-04-2005,"virat@gmail.com",900101012),
-                          (20,"Raju",20-04-2006,"raju@gmail.com",900101013),
-                          (21,"Rani",20-04-2001,"rani@gmail.com",900101050);
+insert into student values(18,"Harsha",'2004-04-20',"harsha@gmail.com",900101010),
+                          (19,"virat",'2005-05-20',"virat@gmail.com",900101012),
+                          (20,"Raju",'2006-04-20',"raju@gmail.com",900101013),
+                          (21,"Rani",'2007-04-20',"rani@gmail.com",900101050);
 #SingleRowFunctions:
 
 Task1:
